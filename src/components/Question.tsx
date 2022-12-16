09f88dbbb5603204a558e2aed65d47da711e55a8
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-12-16 12:23:33 +0100

    First touch of the question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import rightSide from '../assets/right-side.png';
 
 const Question = () => {
   return (
-    <div className="grid grid-cols-2 gap-8">
+    <div className="grid grid-cols-2 mb-32 pl-16 gap-32">
       <section>
-        <h3 className="font-bai-jamjuree font-semibold text-center text-2xl text-text mt-6 mb-10">
+        <h3 className="font-bai-jamjuree font-semibold text-3xl text-text mt-6 mb-10">
           Have a question? <br /> We are ready to assist
         </h3>
-        <form>
-          <input className="border-b " placeholder="Enter your email"/>
+        <form className="flex flex-col items-start w-10/12 gap-y-8">
+          <input
+            className="border-b bg-transparent border-[#AAAA] focus:outline-none p-3 w-full placeholder:text-[#1D1D1D]"
+            placeholder="Enter your email"
+          />
+          <input
+            className="border-b border-[#AAAA] bg-transparent focus:outline-none p-3 w-full placeholder:text-[#1D1D1D]"
+            placeholder="Type your message"
+          />
         </form>
       </section>
+      <div className="relative">
+        <img
+          src={rightSide}
+          alt="right side"
+          className="h-[520px] -mt-56 absolute right-0"
+        />
+      </div>
     </div>
   );
 };
