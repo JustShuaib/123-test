47d229da6056ebe4fbf013249acd012323fb5556
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-12-17 10:25:03 +0100

    Mobile view for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,58 +5,60 @@ import banner from "../assets/banner.png";
 const Header = () => {
   return (
     <>
-      <div className="grid grid-cols-5 items-center pl-16">
-        <header className="col-span-2 pr-10">
-          <h1 className="text-text font-medium text-5xl font-bai-jamjuree">
+      <div className="grid items-center md:grid-cols-5 md:pl-16">
+        <header className="col-span-2 pl-4 pr-2 md:pr-10">
+          <h1 className="text-center font-bai-jamjuree text-3xl font-medium text-text md:text-left md:text-5xl">
             Get
             <span className="text-primary"> Plumbing </span>
             done on your own terms
           </h1>
-          <p className="text-lg font-poppins text-text my-5">
+          <p className="my-5 font-poppins text-base text-text md:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui
             convallis fusce nulla tincidunt sem iaculis. Amet velit etiam nisi.
           </p>
 
           <label
             htmlFor="email"
-            className="bg-white shadow-input rounded-25 flex items-center justify-between pl-4 p-2 -mr-32"
+            className="flex items-center justify-between rounded-25 bg-white p-2 shadow-input md:-mr-32 md:pl-4"
           >
-            <div className="flex items-center justify-start gap-6">
+            <div className="flex items-center justify-start gap-2 md:gap-6">
               <Mail />
               <input
                 id="email"
                 type="email"
                 placeholder="Enter your email address"
-                className="placeholder:text-[#777777] font-poppins py-3 pr-9 text-base focus:outline-none"
+                className="py-1 font-poppins text-sm placeholder:text-[#777777] focus:outline-none md:py-3 md:pr-9 md:text-base"
               />
             </div>
             <button
               type="submit"
-              className="bg-primary text-base font-poppins text-white font-medium py-4 px-7 rounded-20"
+              className="min-w-max rounded-20 bg-primary px-3 py-2 font-poppins text-xs font-medium text-white md:min-w-fit md:px-7 md:py-4 md:text-base"
             >
               Get Started
             </button>
           </label>
-          <button className="mt-10 ml-4 flex text-ltext text-base items-center gap-x-2 font-bold">
+          <button className="mt-6 flex items-center gap-x-2 text-base font-bold text-ltext md:mt-10 md:ml-4">
             <Play />
             <span>See how it works</span>
           </button>
         </header>
-        <div className="col-span-3 -z-10">
-          <img src={banner} alt="banner" />
+        <div className="col-span-3 md:-z-10">
+          <img src={banner} alt="banner" width="100%" />
         </div>
       </div>
 
-      <div className="flex justify-center font-inter mt-14 items-center">
-        <span className="text-primary text-base font-medium">Everything is easy!</span>
-        <ol className="text-text flex items-center gap-x-14 ml-14">
+      <div className="mt-14 flex flex-col items-center justify-center px-1 font-inter md:flex-row">
+        <span className="mb-2 text-base font-medium text-primary md:mb-0">
+          Everything is easy!
+        </span>
+        <ol className="flex flex-col gap-y-1 text-sm md:text-base items-center text-text md:ml-14 md:flex-row md:gap-x-14">
           {[
             "Post a Task",
             "Set your budget or not",
             "Receive quotes & pick the best provider",
           ].map((item, index) => (
             <li key={index}>
-              <span className="bg-primary font-bai-jamjuree rounded-full text-white w-10 h-10 inline-flex mr-2 items-center justify-center">
+              <span className="mr-2 inline-flex h-6 w-6 items-center justify-center rounded-full  bg-primary font-bai-jamjuree text-white md:h-10 md:w-10">
                 {index + 1}
               </span>
               {item}
