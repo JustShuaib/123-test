e26c0b9477ff8821e7a35d18171c2f1ec5f2dc0f
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-12-17 09:45:10 +0100

    Mobile view for app

--- src/components/MobileApp.tsx
+++ src/components/MobileApp.tsx
@@ -4,26 +4,26 @@ import appImg from "../assets/app.png";
 
 const MobileApp = () => {
   return (
-    <div className="mx-6 mb-32 mt-80 grid justify-between rounded-36 gap-6 bg-white md:mx-auto md:mt-44 md:w-2/3 md:grid-cols-2 md:gap-10 md:p-10 md:pr-0 md:pb-0">
-      <div className="p-6 md:p-0">
+    <div className="mx-6 mb-32 mt-80 grid justify-between gap-6 rounded-36 bg-white md:mx-auto md:mt-44 md:w-2/3 md:grid-cols-2 md:gap-10 md:p-10 md:pr-0 md:pb-0">
+      <div className="p-4 md:p-0">
         <p className="text-center font-bai-jamjuree text-3xl font-semibold text-text md:text-left md:text-4xl">
           Try the mobile App
         </p>
-        <p className="mt-5 mb-10 text-lg text-light-text">
+        <p className="mt-5 mb-10 text-base text-light-text md:text-lg">
           TrueLocal is available on Android and Iphones, download to have it at
           your fingertip.
         </p>
         <ul
           role="group"
-          className="flex flex-col items-center gap-6 md:flex-row"
+          className="flex flex-col items-center gap-4 md:flex-row md:gap-6"
         >
           {["App Store", "Google Play"].map((item, index) => (
             <li key={index}>
-              <button className="flex items-center gap-x-1 rounded-20 bg-primary py-3 px-7 font-poppins font-medium text-white">
+              <button className="flex items-center gap-x-2 rounded-20 bg-primary py-3 px-12 font-poppins font-medium text-white md:gap-x-1 md:px-7">
                 {item === "App Store" ? <Apple /> : <PlayStore />}
                 <span className="text-sm">
                   Available on{" "}
-                  <span className="block text-base font-bold md:inline-block">
+                  <span className="block text-sm font-bold md:inline-block md:text-base">
                     {item}
                   </span>
                 </span>
