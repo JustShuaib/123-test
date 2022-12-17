004b90376c8fb1eb6254d91d0a3762e9b0cfa296
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-12-17 09:28:51 +0100

    Mobile view for app

--- src/components/MobileApp.tsx
+++ src/components/MobileApp.tsx
@@ -1,26 +1,29 @@
-import { ReactComponent as Apple } from '../assets/apple.svg'
-import { ReactComponent as PlayStore } from '../assets/google.svg'
-import appImg from '../assets/app.png'
+import { ReactComponent as Apple } from "../assets/apple.svg";
+import { ReactComponent as PlayStore } from "../assets/google.svg";
+import appImg from "../assets/app.png";
 
 const MobileApp = () => {
   return (
-    <div className="w-2/3 mb-32 mt-44 mx-auto grid grid-cols-2 gap-x-10 justify-between bg-white rounded-36 p-10 pr-0 pb-0">
-      <div>
-        <p className="font-bai-jamjuree text-text font-semibold text-4xl">
+    <div className="mx-6 mb-32 mt-80 grid justify-between rounded-36 gap-6 bg-white md:mx-auto md:mt-44 md:w-2/3 md:grid-cols-2 md:gap-10 md:p-10 md:pr-0 md:pb-0">
+      <div className="p-6 md:p-0">
+        <p className="text-center font-bai-jamjuree text-3xl font-semibold text-text md:text-left md:text-4xl">
           Try the mobile App
         </p>
-        <p className="text-light-text text-lg mt-5 mb-10">
+        <p className="mt-5 mb-10 text-lg text-light-text">
           TrueLocal is available on Android and Iphones, download to have it at
           your fingertip.
         </p>
-        <ul role="group" className="flex items-center gap-x-6">
+        <ul
+          role="group"
+          className="flex flex-col items-center gap-6 md:flex-row"
+        >
           {["App Store", "Google Play"].map((item, index) => (
             <li key={index}>
-              <button className="text-white flex gap-x-1 items-center bg-primary font-medium py-3 px-7 rounded-20 font-poppins">
+              <button className="flex items-center gap-x-1 rounded-20 bg-primary py-3 px-7 font-poppins font-medium text-white">
                 {item === "App Store" ? <Apple /> : <PlayStore />}
                 <span className="text-sm">
-                  Available on
-                  <span className="font-bold inline-block text-base">
+                  Available on{" "}
+                  <span className="block text-base font-bold md:inline-block">
                     {item}
                   </span>
                 </span>
@@ -29,11 +32,15 @@ const MobileApp = () => {
           ))}
         </ul>
       </div>
-      <div className="flex justify-end -mt-20">
-        <img src={appImg} alt="Mobile App" className="rounded-br-36 h-[400px]" />
+      <div className="order-first -mt-20 flex justify-end md:-order-none">
+        <img
+          src={appImg}
+          alt="Mobile App"
+          className="h-[300px] md:h-[400px] md:rounded-br-36"
+        />
       </div>
     </div>
   );
-}
+};
 
-export default MobileApp
+export default MobileApp;
